58d3c9cafc95dec09bb517c0322d3f3ec6120b91
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-01-16 15:36:51 -0800

    added all questions

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 const inquirer = require('inquirer');
 const questions = require('./lib/questions');
+const {Triangle, Square, Circle} = require('./lib/shapes');
+const shape = new Triangle('red')
 
-inquirer.prompt(questions);
+
+console.log(shape);
+
+shape.setColor('blue');
+console.log(shape.render());
+// import colors from 'color-name';
+
+// Work on questions and validation for questions
+class GenerateSVG{
+    static init(){
+        inquirer.prompt(questions).then((answersObj) => {
+            switch (answersObj.choice){
+                case 'circle':
+
+                    break
+                case 'triangle':
+                    break
+                case 'square':
+
+                    break
+                default:
+                    console.log('Error');
+            }
+        });
+    }
+}
+
+
+// GenerateSVG.init();
